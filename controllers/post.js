33e44f43804927467792cb2a75c3b94150a59e65
benatespina <benatespina@gmail.com>
2014-04-14 00:05:14 +0200

    Added tagging functionality

--- controllers/post.js
+++ controllers/post.js
@@ -5,6 +5,18 @@ var mongoose    = require("mongoose"),
     Post        = mongoose.model("Post"),
     validations = require("../utils/validations");
 
+var _tags = function (posts) {
+  var i, j, k = 0, postsLength, tagsLength, tags = new Array();
+
+  for (i = 0, postsLength = posts.length; i < postsLength; i++) {
+    for (j = 0, tagsLength = posts[i].tags.length; j < tagsLength; j++) {
+      tags[k] = posts[i].tags[j].tag;
+      k++;
+    }
+  }
+  return _.uniq(tags);
+};
+
 
 exports.posts = function (req, res, next) {
   Post.find().sort("-created").exec(function (err, posts) {
@@ -30,6 +42,8 @@ exports.post = function(req, res, next, slug) {
 };
 
 exports.all = function (req, res) {
+  var tags, i = 0, length = 0;
+
   Post.find().sort("-created").exec(function (err, posts) {
     if (err) {
       res.render(err, {
@@ -37,14 +51,45 @@ exports.all = function (req, res) {
       });
     }
     else {
-      for (var i = 0, length = posts.length; i < length; i++) {
+      for (length = posts.length; i < length; i++) {
         posts[i].body = validations.truncate(posts[i].body, 0, 500);
       }
-        res.render("index", { posts: posts });
+
+      tags = _tags(posts);
+
+      res.render("index", { posts: posts, tags: tags });
     }
   });
 };
 
 exports.show = function(req, res) {
-  res.render("post", { post: req.post, posts: req.posts });
+  var tags = _tags(req.posts);
+
+  res.render("post", { post: req.post, posts: req.posts, tags: tags });
 };
+
+exports.tag = function(req, res) {
+  var tags, i = 0, length = 0;
+
+  Post.find({ 'tags.tag': req.params.tag }).sort('-created').exec(function (err, posts) {
+    if (err) {
+      res.render(err, {
+        status: 500
+      });
+    }
+    else {
+      for (length = posts.length; i < length; i++) {
+        posts[i].body = validations.truncate(posts[i].body, 0, 500);
+      }
+
+      tags = _tags(req.posts);
+
+      res.render('tag', {
+        taggedPosts: posts,
+        posts: req.posts,
+        tags: tags,
+        tag: req.params.tag
+      });
+    }
+  });
+}
--- routes.js
+++ routes.js
@@ -6,7 +6,9 @@ var Post    = require("./controllers/post"),
 module.exports = function (app) {
   app.get("/", Post.all);
   app.get("/:postSlug", Post.show);
+  app.get('/posts/tag/:tag', Post.tag);
 
   app.param('postSlug', Post.post);
   app.param('postSlug', Post.posts);
+  app.param('tag', Post.posts);
 };
