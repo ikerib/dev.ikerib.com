20dd8caea84b75c09c14e888f5328b39bd3b323c
benatespina <benatespina@gmail.com>
2014-06-12 23:34:27 +0200

    Some minor improvements into user login feature

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,22 @@
 'use strict';
 
+var mongoose    = require("mongoose"),
+    _           = require("lodash"),
+    Post        = mongoose.model("Post"),
+    validations = require("../utils/validations");
+
+var _tags = function (posts) {
+  var i, j, k = 0, postsLength, tagsLength, tags = [];
+
+  for (i = 0, postsLength = posts.length; i < postsLength; i++) {
+    for (j = 0, tagsLength = posts[i].tags.length; j < tagsLength; j++) {
+      tags[k] = posts[i].tags[j].tag;
+      k++;
+    }
+  }
+  return _.uniq(tags);
+};
+
 exports.login = function (req, res) {
   res.render('admin/login', {message: req.flash('loginMessage')});
 };
@@ -18,7 +35,22 @@ exports.isLoggedIn = function (req, res, next) {
 };
 
 exports.dashboard = function (req, res) {
-  res.render('admin/dashboard', {
-    user: req.user
+  var tags, i = 0, length = 0;
+
+  Post.find().sort("-created").exec(function (err, posts) {
+    if (err) {
+      res.render(err, {
+        status: 500
+      });
+    }
+    else {
+      for (length = posts.length; i < length; i++) {
+        posts[i].body = validations.truncate(posts[i].body, 0, 500);
+      }
+      tags = _tags(posts);
+      res.render('admin/dashboard', {
+        user: req.user, posts: posts, tags: tags
+      });
+    }
   });
 };
