6890c71d02d516349db09369d9b67072d484718d
benatespina <benatespina@gmail.com>
2014-06-22 12:15:05 +0200

    Added config files Heroku's deployment and some code improvements

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,17 @@
-"use strict";
+'use strict';
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-compass");
-  grunt.loadNpmTasks("grunt-contrib-cssmin");
-  grunt.loadNpmTasks("grunt-mocha-cov");
-  grunt.loadNpmTasks("grunt-nodemon");
-  grunt.loadNpmTasks("grunt-concurrent");
-  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
     meta: {
-      file: "benatespina-blog",
+      file: 'benatespina-blog',
       banner: '/* <%= meta.file %> v<%= pkg.version %> - '                    +
               '<%= grunt.template.today("yyyy/m/d") %>\n'                     +
               '<%= grunt.template.today("yyyy") %> <%= pkg.author.name %>'    +
@@ -23,57 +22,60 @@ module.exports = function(grunt) {
         livereload: true
       },
       compass: {
-        files: "assets/stylesheets/**/*.scss",
-        tasks: ["compass", "cssmin"]
+        files: 'assets/stylesheets/**/*.scss',
+        tasks: ['compass', 'cssmin']
       },
       jade: {
-        files: "views/**/*.jade"
+        files: 'views/**/*.jade'
       }
     },
     compass: {
       compile: {
         options: {
-          sassDir: "assets/stylesheets",
-          cssDir : "public/css"
+          sassDir: 'assets/stylesheets',
+          cssDir : 'public/css'
         }
       }
     },
     cssmin: {
       minify: {
         options: {
-          banner: "<%= meta.banner %>"
+          banner: '<%= meta.banner %>'
         },
         files: [
           {
             expand: true,
-            cwd: "public/css/",
-            src: "*.css",
-            dest: "public/",
-            ext: ".min.css"
+            cwd: 'public/css/',
+            src: '*.css',
+            dest: 'public/',
+            ext: '.min.css'
           }
         ]
       }
     },
-    clean: ["public"],
+    clean: {
+      dev: ['public'],
+      prod: ['public/css']
+    },
     copy: {
       images: {
         files: [
           {
             expand: true,
-            cwd: "assets/img",
-            src: "**/*",
-            dest: "public/img",
-            filter: "isFile"
+            cwd: 'assets/img',
+            src: '**/*',
+            dest: 'public/img',
+            filter: 'isFile'
           }
         ]
       }
     },
     nodemon: {
       dev: {
-        script: "server.js",
+        script: 'server.js',
         options: {
-          ignore: ["README.md", "node_modules/**", ".DS_Store"],
-          watch: ["server"],
+          ignore: ['README.md', 'node_modules/**', '.DS_Store'],
+          watch: ['server'],
           debug: true,
           delayTime: 1,
           env: {
@@ -84,19 +86,19 @@ module.exports = function(grunt) {
       }
     },
     concurrent: {
-      tasks: ["nodemon:dev", "watch"],
+      tasks: ['nodemon:dev', 'watch'],
       options: {
         logConcurrentOutput: true
       }
     }
   });
-  grunt.registerTask("default", "Main Grunt. Executes all tasks.", [
-    "clean", "copy", "stylesheets", "concurrent"
+  grunt.registerTask('default', 'Main Grunt. Executes all tasks.', [
+    'clean', 'copy', 'stylesheets', 'concurrent'
   ]);
-  grunt.registerTask("stylesheets", "Compiles the stylesheets.",   [
-    "compass", "cssmin"
+  grunt.registerTask('prod', 'Grunt for production environment.',  [
+    'clean', 'copy', 'stylesheets', 'clean:prod'
   ]);
-  grunt.registerTask("test", "Executes the tests of project.",     [
-    "mochacov"
+  grunt.registerTask('stylesheets', 'Compiles the stylesheets.',   [
+    'compass', 'cssmin'
   ]);
 };
