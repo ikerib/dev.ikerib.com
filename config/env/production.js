d16b2af5cbd34a1c0765a6fd5dc943a80bfb04ea
Iker Ibarguren <ikerib@gmail.com>
2014-09-17 11:08:25 +0200

    produccion.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,9 @@
 'use strict';
 // var MONGOHQ_URL="mongodb://ikerdev:berahero8>@kahana.mongohq.com:10021/ikerdev"
-var connectionUri = url.parse(process.env.MONGOHQ_URL);
-var dbName = connectionUri.pathname.replace(/^\//, '');
+// var url = require('url');
+// var connectionUri = url.parse(process.env.MONGOHQ_URL);
+// var dbName = connectionUri.pathname.replace(/^\//, '');
+console.log(process.env.MONGOHQ_URL);
 module.exports = {
   db    : process.env.MONGOHQ_URL,
   secret: 'mysessionsecretkey',
