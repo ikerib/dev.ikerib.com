d188af1db5e477943a15fe0ed0f447605c36abb0
Iker Ibarguren <ikerib@gmail.com>
2014-09-17 10:00:34 +0200

    produccion dist

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
-
+// mongodb://<user>:<password>@kahana.mongohq.com:10021/ikerdev
 module.exports = {
-  db    : 'mongodb://myproductionmongodbinstance',
+  db    : 'mongodb://ikerib:PASSWORD@kahana.mongohq.com:10021/ikerdev',
   secret: 'mysessionsecretkey',
   app   : {
     name: 'ikerdevbloga'
