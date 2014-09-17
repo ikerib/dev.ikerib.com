76fadc910bdb409f798b4149d77a1e20c564b59f
Iker Ibarguren <ikerib@gmail.com>
2014-09-17 10:56:08 +0200

    produccion.js

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 var MONGOHQ_URL="mongodb://ikerdev:berahero8>@kahana.mongohq.com:10021/ikerdev"
 module.exports = {
-  db    : process.env.('MONGOHQ_URL'),
+  db    : MONGOHQ_URL,
   secret: 'mysessionsecretkey',
   app   : {
     name: 'ikerdevbloga'
